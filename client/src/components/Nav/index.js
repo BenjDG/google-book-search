7c24fa7537786b1f705ea5b826c27fcd6eb7d55a
Ben Galloway <bdgalloway85@gmail.com>
2021-02-18 01:25:33 -0600

    change nav styles

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,9 +3,22 @@ import React from 'react';
 function Nav () {
   return (
     <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
-      <a className='navbar-brand' href='/'>
-        React Reading List
-      </a>
+      <div class='container-fluid'>
+        <h1 className='navbar-brand'>Google Books</h1>
+        <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+          <span class='navbar-toggler-icon' />
+        </button>
+        <div class='collapse navbar-collapse' id='navbarNav'>
+          <ul class='navbar-nav'>
+            <li class='nav-item'>
+              <a class='nav-link' href='/'>Search</a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='/saved'>Saved</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
